cc3566f089c30f669a42f7526edb2cd5de70f5ac
Dennis <dennisdiepolder@outlook.com>
2024-04-18 20:02:51 +0200

    typescript

--- src/app/page.js
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="flex flex-col w-full h-screen justify-center items-center">
-      <h1 className="text-4xl font-bold">Freihaus</h1>
-    </div>
-  );
-}
