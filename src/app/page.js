7000f9755e7de7479cac476734c3c17c7c23d694
Dennis <dennisdiepolder@outlook.com>
2024-04-18 14:45:36 +0200

    second

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,8 @@
-import RotatingCube from "@/components/3dhouse/3dhouse";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full h-screen justify-center items-center">
-      <h1 className="text-5xl font-bold mt-20">3D House</h1>
-      <RotatingCube />
-      <h1>test</h1>
+      <h1 className="text-4xl font-bold">Freihaus</h1>
     </div>
   );
 }
